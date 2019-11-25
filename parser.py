50cc0f0d4d27218a9abdc3a53edd50ed1707388b
proton <chadapohn.chaosrikul@gmail.com>
2019-11-25 20:09:38 +0700

    parse chrom_hgvs_names, starts, ends

--- parser.py
+++ parser.py
@@ -12,6 +12,11 @@ CHROM_ROW = 3
 CHROM_COL = 1
 CHROM_PATTERN = r"^.*N\w_\s*(\d+)\.\d+"
 
+VARIANT_ROW = 3
+VARIANT_COL = 2
+IN_OR_DEL_PATTERN = r"^[cgp]\.(\d+\_\d+).*$"
+SNP_PATTERN = r"^[cgp]\.(\d+).*$"
+
 def parse_gene(gene_cell):
     gene = None
     match = re.match(GENE_PATTERN, gene_cell)
@@ -20,7 +25,7 @@ def parse_gene(gene_cell):
     return gene
 
 def parse_chrom(chrom_cell):
-    chrom_num = None
+    chrom_name = None
     match = re.match(CHROM_PATTERN, chrom_cell)
     if match:
         chrom_num = int(match.group(1))
@@ -30,10 +35,38 @@ def parse_chrom(chrom_cell):
             chrom_name = "chrY"
         else:
             chrom_name = "chr" + str(chrom_num)
-    return chrom_num
+    return chrom_name
+
+def parse_variants(variant_cells, num_variants):
+    starts = []
+    ends = []
+    chrom_hgvs_names = []
+    for var_idx in range(VARIANT_COL, num_variants):
+        chrom_hgvs_name = variant_cells.loc[var_idx]
+        match_in_or_del_pattern = re.match(IN_OR_DEL_PATTERN, chrom_hgvs_name)
+        match_snp_pattern = re.match(SNP_PATTERN, chrom_hgvs_name)
+        if match_in_or_del_pattern:
+            start = int(match_in_or_del_pattern.group(1).split("_")[0]) - 1
+            end = int(match_in_or_del_pattern.group(1).split("_")[1])
+        elif match_snp_pattern:
+            end = int(match_snp_pattern.group(1))
+            start = end - 1
+        else:
+            chrom_hgvs_name = None
+            start = None
+            end = None
+        chrom_hgvs_names.append(chrom_hgvs_name)
+        starts.append(start)
+        ends.append(end)
+
+    return chrom_hgvs_names, starts, ends
         
+
 if __name__ == "__main__":
     definition_file = path.join(HAPLOTYPE_TABLE_DIR, "G6PD_allele_definition_table.xlsx")
     definition_table = pd.read_excel(definition_file, header=None)
     gene = parse_gene(definition_table.iloc[GENE_ROW][GENE_COL])
     chrom = parse_chrom(definition_table.iloc[CHROM_ROW][CHROM_COL])
+    num_variants = definition_table.iloc[CHROM_ROW].count() - 1
+    chrom_hgvs_names, starts, ends = parse_variants(definition_table.iloc[VARIANT_ROW, VARIANT_COL:num_variants+VARIANT_COL], num_variants)
+   
